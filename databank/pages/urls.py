8b5a573284976b38a0cb2a927580a8c1332cc8d1
topherst <topheress@gmail.com>
2021-10-03 07:25:20 -0700

    note links

--- databank/pages/urls.py
+++ databank/pages/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('notes/', views.notes, name="notes"),
+    path('about/', views.about, name="about"),
+    path('cs/', views.cs, name="cs"),
+    path('history/', views.history, name="history"),
+    path('politics/', views.politics, name="politics"),
+    path('philosophy/', views.philosophy, name="philosophy"),
+    
 ]
--- databank/pages/views.py
+++ databank/pages/views.py
@@ -7,3 +7,23 @@ def home(request):
 def notes(request):
     context = {}
     return render(request, 'pages/notes.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'pages/about.html', context)
+
+def cs(request):
+    context = {}
+    return render(request, 'pages/cs.html', context)
+
+def history(request):
+    context = {}
+    return render(request, 'pages/history.html', context)
+
+def politics(request):
+    context = {}
+    return render(request, 'pages/politics.html', context)
+
+def philosophy(request):
+    context = {}
+    return render(request, 'pages/philosophy.html', context)
